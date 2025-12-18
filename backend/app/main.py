c05336ec654445c1dd7d51b54af45b39ca1f79ed
Corianne1172 <otiohkonan@gmail.com>
2025-12-18 17:48:42 -0600

    Complete user authentication with JWT and Argon2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-
 from app.db.database import engine
 from app.db.base import Base
-from app.models import models  # IMPORTANT: imports models
+from app.models import models  # noqa
+from app.routes.auth import router as auth_router
 
 app = FastAPI(title="BiteBoxd API")
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth_router)
 
 @app.get("/health")
 def health():
